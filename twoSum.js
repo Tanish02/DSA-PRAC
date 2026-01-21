ac98ce9416ecceac6a87f197707119e9bb4adaf2
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-01-21 13:27:22 +0530

    twoSum with hashMap

--- twoSum.js
+++ twoSum.js
@@ -13,15 +13,49 @@
 //}
 
 //////////////////////code/////////////////////
+// function twoSum() {
+//   const arr = [2, 4, 6, 8, 1, 3, 5, 7];
+//   const target = 9;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === target) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+// console.log("Result:", twoSum());
+
+//===========================================================
+
+// two sum problem with hashMap approach
+/////////////////////////logic//////////////////
+
+// function twoSum () {
+// const array and
+//create an empty hashMap and traverse
+// calculate required value = target - current element
+// if required value exist in msp
+// return true
+// else
+// store current element in map
+// after loop return false
+// }
+
+//////////////////////code/////////////////////
+
 function twoSum() {
-  const arr = [2, 4, 6, 8, 1, 3, 5, 7];
-  const target = 9;
+  const arr = [2, 4, 6, 8, 1, 3, 5];
+  const target = 90;
+  let map = new Map();
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === target) {
-        return true;
-      }
+    let needed = target - arr[i];
+
+    if (map.has(needed)) {
+      return true;
     }
+    map.set(arr[i], true);
   }
   return false;
 }
