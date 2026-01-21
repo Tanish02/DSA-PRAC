532c2bd78afb5ac5d4be888b091a78ae87c98620
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-01-21 12:57:44 +0530

    two sum problem

--- twoSum.js
+++ twoSum.js
@@ -14,7 +14,7 @@
 
 //////////////////////code/////////////////////
 function twoSum() {
-  const arr = [1, 3, 5, 7, 2, 4, 6];
+  const arr = [2, 4, 6, 8, 1, 3, 5, 7];
   const target = 9;
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
@@ -25,5 +25,4 @@ function twoSum() {
   }
   return false;
 }
-console.log("Result :", twoSum());
-twoSum();
+console.log("Result:", twoSum());
