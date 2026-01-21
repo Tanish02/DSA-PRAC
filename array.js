73536197a7519ecdf754924c44bea78cb12865ca
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-01-21 12:34:30 +0530

    init repo

---
+++ array.js
@@ -0,0 +1,18 @@
+// // //
+
+// function printArry(arr) {
+//   // gothrough each element of an arry
+//   for (let i = 0; i < arr.length; i++) {
+//     // and print it to the console
+//     console.log(arr[i]);
+//   }
+// }
+
+function print() {
+  const arr = [1, 2, 3, 4, 5];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+print();
