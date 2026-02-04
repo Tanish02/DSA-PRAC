d8037825e250d66170c4c70dfe21a10cbdfb1790
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-02-05 02:08:44 +0530

    update additinal logic

--- twoSum.js
+++ twoSum.js
@@ -4,7 +4,7 @@
 // function twoSum () {
 // const arr = [array elements]
 // const target = [target sum]
-// traverse through array and setb index to 0 = i=0 with for loop
+// traverse through array and set b index to 0 = i=0 with for loop
 // nested for loop set to + 1 = j= i+1
 // check if arr[i] + arr[j] === target
 // if true return true
@@ -45,18 +45,50 @@
 
 //////////////////////code/////////////////////
 
-function twoSum() {
-  const arr = [2, 4, 6, 8, 1, 3, 5];
-  const target = 90;
-  let map = new Map();
-  for (let i = 0; i < arr.length; i++) {
-    let needed = target - arr[i];
+// function twoSum() {
+//   const arr = [2, 4, 6, 8, 1, 3, 5];
+//   const target = 90;
+//   let map = new Map();
+//   for (let i = 0; i < arr.length; i++) {
+//     let needed = target - arr[i];
 
-    if (map.has(needed)) {
-      return true;
+//     if (map.has(needed)) {
+//       return true;
+//     }
+//     map.set(arr[i], true);
+//   }
+//   return false;
+// }
+// console.log("Result:", twoSum());
+
+// function twoSum() {
+//   const arr = [2, 4, 6, 8, 1, 3, 5, 7];
+//   const target = 9;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === target) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+// console.log("Result:", twoSum());
+
+////////////////////// RETURN INDICES //////////////////
+// return index number with result
+function Sum() {
+  const arr = [10, 12, 18, 6, 4];
+  const target = 30;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        console.log("Indices: ", i, j);
+        return true;
+      }
     }
-    map.set(arr[i], true);
   }
   return false;
+  console.log("No pair Found in this array");
 }
-console.log("Result:", twoSum());
+console.log("Result: ", Sum());
