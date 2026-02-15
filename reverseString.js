470b25c2aa5d5cc50cb2a71ec4713515956342af
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-02-16 04:12:12 +0530

    completed

--- reverseString.js
+++ reverseString.js
@@ -1,3 +1,14 @@
+/// =====================================LOGIC===============================================
+// let str = "Tanish kumar sharma"; // default string
+// // 1st split default string
+// // use .map for record character location
+// let str1 = str.split("").reverse("").join(""); // join characters into words
+// // join again for words to join into sentence
+// console.log(str1);
+//
+// logic order <=====> string → split->(convert into array)→ map → modify elements -> print
+
+//===========================================================================================
 // reverse a string  ["Tanish"]
 // const arr = ["Tanish"];
 // let str = arr[0].split("").reverse().join("");
@@ -15,8 +26,22 @@
 // console.log(str); // hsinaT si eman yM
 
 //
-let str = "my name is tanish";
+// let str = "my name is tanish";
+// let str1 = str.split(" ");
+// let str2 = str1.map((str1) => str1.split("").reverse().join(""));
+// let str3 = str2.join(" ");
+// console.log(str3);
+
+// let str = "tanish is my name";
+// let str1 = str.split(" "); // str split and convert into array
+// let str2 = str1.map((str1) => str1.split("").reverse("").join("")); // str1 mapping & split then reverse and join
+// var str3 = str2.join(" ");
+// console.log("Result :", str3);
+
+let str = "My name is Tanish";
 let str1 = str.split(" ");
-let str2 = str1.map((str1) => str1.split("").reverse().join(""));
+// console.log(str1);
+let str2 = str1.map((str1) => str1.split("").reverse("").join(""));
+// console.log(str2);
 let str3 = str2.join(" ");
 console.log(str3);
