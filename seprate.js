74d2eee33d0c47ab5657b81c4d1d5d913f4d72d9
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-03-18 02:23:57 +0530

    seprate problem

--- seprate.js
+++ seprate.js
@@ -13,3 +13,49 @@
 //   return [letter, number];
 // };
 // console.log(sep());
+
+// const sep = (arr) => {
+//   let letters = [];
+//   let numbers = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!isNaN(arr[i])) {
+//       numbers.push(arr[i]);
+//     } else {
+//       letters.push(arr[i]);
+//     }
+//   }
+//   return [letters, numbers];
+// };
+// console.log(sep(["a", "1", "b", "2", "c", "3", ""]));
+
+const a = "aB3@cD1#100";
+// lower case = "a to z "
+// upper case = "A to Z"
+// number = "1 to 1000"
+// symbol = everything else
+
+const sep = () => {
+  let alphabet = {
+    lower: [],
+    upper: [],
+  };
+  let number = [];
+  let symbol = [];
+  let str = [...a];
+
+  for (let i = 0; i < str.length; i++) {
+    let k = str[i];
+
+    if (k >= "a" && k <= "z") {
+      alphabet.lower.push(k);
+    } else if (k >= "A" && k <= "Z") {
+      alphabet.upper.push(k);
+    } else if (k >= "1" && k <= "9") {
+      number.push(k);
+    } else {
+      symbol.push(k);
+    }
+  }
+  return [alphabet, number, symbol];
+};
+console.log(sep());
