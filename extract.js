de9f7d5ee50bd431dce2de01524d4928c5cfe824
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-03-19 18:01:11 +0530

    wip: for add sum of digit from string

--- extract.js
+++ extract.js
@@ -1,33 +1,39 @@
 // extract digits from string and sum
-const str = "ab12cd034ef5";
 
-const sum = () => {
-  let num = 0;
-  let current = "";
-  for (let i = 0; i < str.length; i++) {
-    let k = str[i];
+const str = "a12b3c45";
+
+const fun = () => {
+  let alphabet = { upper: [], lower: [] };
+  let digit = [];
+  let symbol = [];
+  let temp = "";
+  let sum = 0;
+
+  for (let k of str) {
+    //digit
     if (k >= "0" && k <= "9") {
-      current += k; // build number
+      temp += k;
     } else {
-      if (current !== "") {
-        num += Number(current);
-        current = "";
+      if (temp !== "") {
+        let value = Number(temp);
+        digit.push(value);
+        sum += value;
+        temp = "";
+      }
+      if (k >= "A" && k <= "Z") {
+        alphabet.upper.push(k);
+      } else if (k >= "a" && k <= "z") {
+        alphabet.lower.push(k);
+      } else {
+        symbol.push(k);
       }
     }
   }
-  // last number
-  if (current !== "") {
-    num += Number(current);
-  }
-  return num;
+  // if (temp !== "") {
+  //   let value = Number(temp);
+  //   digit.push(value);
+  //   sum += value;
+  // }
+  return { alphabet, digit, symbol, sum };
 };
-console.log(sum());
-
-// const str = "ab12cd034ef5";
-
-// function sum() {
-//   return (str.match(/-?\d+/g) || [])
-//     .reduce((acc, num) => acc + Number(num), 0);
-// }
-
-// console.log(sum()); // 51
+console.log(fun());
