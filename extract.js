2132c2186ac08cc3edd3fcb4fb1e1096bc0969aa
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-03-21 14:44:14 +0530

    sum from string practice

--- extract.js
+++ extract.js
@@ -1,39 +1,87 @@
-// extract digits from string and sum
+// // extract digits from string and sum
 
-const str = "a12b3c45";
+// const str = "a12b3c45";
 
-const fun = () => {
-  let alphabet = { upper: [], lower: [] };
-  let digit = [];
-  let symbol = [];
+// const fun = () => {
+//   let alphabet = { upper: [], lower: [] };
+//   let digit = [];
+//   let symbol = [];
+//   let temp = "";
+//   let sum = 0;
+
+//   for (let k of str) {
+//     //digit
+//     if (k >= "0" && k <= "9") {
+//       temp += k;
+//     } else {
+//       if (temp !== "") {
+//         let value = Number(temp);
+//         digit.push(value);
+//         sum += value;
+//         temp = "";
+//       }
+//       if (k >= "A" && k <= "Z") {
+//         alphabet.upper.push(k);
+//       } else if (k >= "a" && k <= "z") {
+//         alphabet.lower.push(k);
+//       } else {
+//         symbol.push(k);
+//       }
+//     }
+//   }
+//   // if (temp !== "") {
+//   //   let value = Number(temp);
+//   //   digit.push(value);
+//   //   sum += value;
+//   // }
+//   return { alphabet, digit, symbol, sum };
+// };
+// console.log(fun());
+
+/////////////////////// REGEX Method///////////////////////////////////////////////
+//=================================================================
+// const str = "a-10b20c-003a+2d5";
+
+// const sum = () => {
+//   const number = str.match(/[-+]?\d+/g);
+//   let sum = 0;
+//   for (let num of number) {
+//     sum += Number(num);
+//   }
+//   return sum;
+// };
+// console.log(sum());
+
+//=========================================================
+
+// ===========================       practice       ===============================
+
+const str = "tanish12sh23ar67ma777";
+// extarct number
+
+const num = () => {
+  let number = [];
   let temp = "";
   let sum = 0;
 
   for (let k of str) {
-    //digit
     if (k >= "0" && k <= "9") {
       temp += k;
     } else {
       if (temp !== "") {
         let value = Number(temp);
-        digit.push(value);
+        number.push(value);
         sum += value;
         temp = "";
       }
-      if (k >= "A" && k <= "Z") {
-        alphabet.upper.push(k);
-      } else if (k >= "a" && k <= "z") {
-        alphabet.lower.push(k);
-      } else {
-        symbol.push(k);
-      }
     }
   }
-  // if (temp !== "") {
-  //   let value = Number(temp);
-  //   digit.push(value);
-  //   sum += value;
-  // }
-  return { alphabet, digit, symbol, sum };
+  if (temp !== "") {
+    let value = Number(temp);
+    number.push(value);
+    sum += value;
+    temp = "";
+  }
+  return { number, temp, sum };
 };
-console.log(fun());
+console.log(num());
