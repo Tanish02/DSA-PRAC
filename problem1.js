3010499ea9d3e82649e7d31b0257c14f6fde7347
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-03-22 19:28:40 +0530

    solving problems1

--- problem1.js
+++ problem1.js
@@ -72,25 +72,129 @@
 // digit: [1]
 // symbol: [@]
 
-const str = "aB1@cD";
+// const str = "aB1@cD";
 
-const separate = () => {
-  let lower = [];
-  let upper = [];
-  let digit = [];
-  let symbol = [];
+// const separate = () => {
+//   let lower = [];
+//   let upper = [];
+//   let digit = [];
+//   let symbol = [];
 
-  for (let k of str) {
+//   for (let k of str) {
+//     if (k >= "a" && k <= "z") {
+//       lower.push(k);
+//     } else if (k >= "A" && k <= "Z") {
+//       upper.push(k);
+//     } else if (k >= "0" && k <= "9") {
+//       digit.push(k);
+//     } else {
+//       symbol.push(k);
+//     }
+//   }
+//   return { lower, upper, digit, symbol };
+// };
+// console.log(separate());
+
+//============================================================
+
+// 4 Input: "abCdeF"
+// Output: "C"
+
+// const select = () => {
+//   let str = "abCdeF";
+//   let Output = [];
+
+//   for (let k of str) {
+//     if (k >= "A" && k <= "Z") {
+//       return k;
+//     }
+//   }
+//   return null;
+// };
+// console.log(select());
+
+//===========================================
+// 5 Input: "a1B2c"
+// Output: "A1b2C"
+// const str = "a1B2c";
+// const switchchar = () => {
+//   let str1 = "";
+//   for (let k of str) {
+//     if (k >= "a" && k <= "z") {
+//       str1 += k.toUpperCase();
+//     } else if (k >= "A" && k <= "Z") {
+//       str1 += k.toLowerCase();
+//     } else {
+//       str1 += k;
+//     }
+//   }
+//   return str1;
+// };
+// console.log(switchchar());
+
+//==========================================
+// 6 Remove Uppercase
+// "aBcDeF" → "ace"
+
+// const str = "aBcDeF";
+// const select = (str) => {
+//   let result = "";
+//   for (let k of str) {
+//     if (k >= "a" && k <= "z") {
+//       result += k;
+//     }
+//   }
+//   return result;
+// };
+// console.log(select("aBcDeF"));
+
+//=============================
+// 7 Replace Lowercase with *
+// "aBcD" → "*B*D"
+//7.1 Input: "aBcD";
+// Output: "a#c#";
+//7.2 Input: "a1B2c3";
+// Output: "a*B*c*";
+
+// 7
+const str1 = "aBcD";
+const replace7 = () => {
+  let result = "";
+  for (let k of str1) {
     if (k >= "a" && k <= "z") {
-      lower.push(k);
-    } else if (k >= "A" && k <= "Z") {
-      upper.push(k);
-    } else if (k >= "0" && k <= "9") {
-      digit.push(k);
+      result += "*";
+    } else result += k;
+  }
+  return result;
+};
+console.log(replace7());
+
+// 7.1
+const str2 = "aBcD";
+const replace7_1 = () => {
+  let result1 = "";
+  for (let k of str2) {
+    if (k >= "A" && k <= "Z") {
+      result1 += "#";
     } else {
-      symbol.push(k);
+      result1 += k;
     }
   }
-  return { lower, upper, digit, symbol };
+  return result1;
 };
-console.log(separate());
+console.log(replace7_1());
+
+// 7.2
+const str3 = "a1B2c3";
+const replace7_3 = () => {
+  let result2 = "";
+  for (let k of str3) {
+    if (k >= "0" && k <= "9") {
+      result2 += "*";
+    } else {
+      result2 += k;
+    }
+  }
+  return result2;
+};
+console.log(replace7_3());
