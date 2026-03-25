9fb69e1cd021e45064db925abc50b86059ea1a97
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-03-26 04:00:52 +0530

    problem2: non repeating char

--- problem2.js
+++ problem2.js
@@ -40,28 +40,68 @@
 // Output: "abcde"
 
 // approach 1
-const removedup1 = (str) => [...new Set(str)].join("");
-console.log(removedup1("aabbccdde"));
+// const removedup1 = (str) => [...new Set(str)].join("");
+// console.log(removedup1("aabbccdde"));
 
-// approach 2
-const removedup = (str) => {
-  let seen = {};
-  let result = "";
+// // approach 2
+// const removedup = (str) => {
+//   let seen = {};
+//   let result = "";
 
+//   for (let k of str) {
+//     if (!seen[k]) {
+//       seen[k] = true;
+//       result += k;
+//     }
+//   }
+//   return result;
+// };
+// console.log(removedup("aabbccdde"));
+
+// // approach 3
+// let str2 = "testing"; // tesing
+// let arr1 = str2.split("");
+// let value = new Set(arr1);
+// let arr2 = [...value];
+// let str3 = arr2.join("");
+// console.log(str3);
+
+//==================================================
+// 🟢 2. First Non-Repeating Character
+// Input: "aabbcdd"
+// Output: "c"
+
+// approach 1 = only show 1st non repeat char
+const nonrepeatchar = (str) => {
+  let check = {};
   for (let k of str) {
-    if (!seen[k]) {
-      seen[k] = true;
-      result += k;
+    check[k] = (check[k] || 0) + 1;
+  }
+  for (let k of str) {
+    if (check[k] === 1) {
+      return k;
     }
   }
-  return result;
+  return null;
 };
-console.log(removedup("aabbccdde"));
+console.log(nonrepeatchar("aabbcddeff"));
 
-// approach 3
-let str2 = "testing"; // tesing
-let arr1 = str2.split("");
-let value = new Set(arr1);
-let arr2 = [...value];
-let str3 = arr2.join("");
-console.log(str3);
+// approach 2 = show All non-repeating char
+const nonrepeatchar2 = (strr) => {
+  let check = {};
+  let result = [];
+  for (let k of strr) {
+    check[k] = (check[k] || 0) + 1;
+  }
+  for (let k of strr) {
+    if (check[k] === 1) {
+      result.push(k);
+    }
+  }
+  if (result.length > 0) {
+    return result;
+  } else {
+    return null;
+  }
+};
+console.log(nonrepeatchar2, "aabbcddeff");
